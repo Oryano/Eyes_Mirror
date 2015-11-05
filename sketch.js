45a5d569ece1076150911027b3afb01be224d5af
Oryan Inbar <oryan.inbar@gmail.com>
2015-11-05 15:07:46 -0500

    adds array of circles to gh-pages

--- sketch.js
+++ sketch.js
@@ -1,54 +1,103 @@
-
 var ctracker;
-var ripple = 0;
+var ripples = [];
+
+
 function setup() {
   // setup camera capture
    var videoInput = createCapture(VIDEO);
-   videoInput.size(400, 300);
+   videoInput.size(600, 400);
    videoInput.position(0, 0);
-   videoInput.style('transform: rotateY(180deg)'); //wont be neded when video.hide()
+   videoInput.style('transform: rotateY(180deg)'); //wont be needed when video.hide()
    //videoInput.hide(); 
 
   // setup canvas
-  var cnv = createCanvas(400, 300);
+  var cnv = createCanvas(600, 400);
   cnv.position(0, 0);
-  //do like a mirror
+  //flip to be like a mirror
   cnv.style('transform: rotateY(180deg)');
   // setup tracker
   ctracker = new clm.tracker();
   ctracker.init(pModel);
   ctracker.start(videoInput.elt);
-  noStroke();
+
+  noStroke();  //needed?
 }
 
+
+
 function draw() {
-  clear();
+  background(0,0,0);
+  //clear();
   // get array of face marker positions [x, y] format
   var positions = ctracker.getCurrentPosition();
   
-  for (var i = 0; i < positions.length; i++) {
-    //console.log("y = " + positions[27][0]);
+  for (var i = 0; i < positions.length; i++) {  //if(positions){ ALSO WORKS BUT WHY?
     // draw ellipse at eyes position point [27], [32]
     var leftEyeX = positions[27][0];  //array index 27 y
     var leftEyeY = positions[27][1];  //array index 27 x
     ellipseMode(CENTER);
-    ellipse(leftEyeX, leftEyeY, 20, 10);
+    ellipse(leftEyeX, leftEyeY, 10, 10);
     
     var rightEyeX = positions[32][0];
     var rightEyeY = positions[32][1];
-    ellipseMode(CENTER);
-    fill(0);
-    ellipse(rightEyeX, rightEyeY, 20, 10);
-    
-    //console.log("left eye pos = " + round(leftEyeX) + " " +round(leftEyeY));
+    ellipse(rightEyeX, rightEyeY, 10, 10);
+  }  
+
+  //loop thru whats in ripples[], display and changeDiam each
+  for (var i = 0; i < ripples.length; i ++ ) { 
+    ripples[i].changeDiam();
+    ripples[i].display();
+    //remove the first one (oldest)
+    if(ripples[i].lifespan < 0){
+    ripples.splice(i, 1);    //splice = remove
+   }
+  }
+  
+  var leftEyeRipple = new rippelFromEye(leftEyeX,leftEyeY);
+  ripples.push(leftEyeRipple);
+  var rightEyeRipple = new rippelFromEye(rightEyeX,rightEyeY);
+  ripples.push(rightEyeRipple);
+
+}
+
+
+//------------------------------------------function to make ripples from ONE source
+function rippelFromEye(eyePosX, eyePosY, diam){
+  diam = 50;
+  this.lifespan = 255;
+
+  this.changeDiam = function() {
+    //if (diam < 150){
+    diam++;
+    this.lifespan = this.lifespan - 4;
+   // }
   }
 
-  ripple++;
-  stroke(255,0,0);
+  this.display = function() {
+  //stroke(hue(ballCol), saturation(ballCol), brightness(ballCol), this.lifespan);
+  stroke(180, 20 , 80, this.lifespan);
+  strokeWeight(2);
   noFill();
-  ellipse(leftEyeX, leftEyeY, ripple, ripple);
-  ellipse(rightEyeX, rightEyeY, ripple, ripple);
+  ellipse(eyePosX, eyePosY, diam, diam);
+  }
 
-if (ripple > 100) ripple = 0;
-  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
