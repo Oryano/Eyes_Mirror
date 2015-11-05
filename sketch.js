32d186b425d03ef0f3a618276eb1456a4be025ee
Oryan Inbar <oryan.inbar@gmail.com>
2015-11-05 01:42:03 -0500

    commitign to new branch circles from eyes

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
-
 var ctracker;
 var ripple = 0;
+
+
 function setup() {
   // setup camera capture
    var videoInput = createCapture(VIDEO);
-   videoInput.size(400, 300);
+   videoInput.size(600, 400);
    videoInput.position(0, 0);
-   videoInput.style('transform: rotateY(180deg)'); //wont be neded when video.hide()
+   videoInput.style('transform: rotateY(180deg)'); //wont be needed when video.hide()
    //videoInput.hide(); 
 
   // setup canvas
-  var cnv = createCanvas(400, 300);
+  var cnv = createCanvas(600, 400);
   cnv.position(0, 0);
   //do like a mirror
   cnv.style('transform: rotateY(180deg)');
@@ -18,37 +19,56 @@ function setup() {
   ctracker = new clm.tracker();
   ctracker.init(pModel);
   ctracker.start(videoInput.elt);
-  noStroke();
+
+  noStroke();  //needed?
 }
 
+
+
 function draw() {
-  clear();
+  clear();  //like backround in draw
   // get array of face marker positions [x, y] format
   var positions = ctracker.getCurrentPosition();
   
-  for (var i = 0; i < positions.length; i++) {
-    //console.log("y = " + positions[27][0]);
+  for (var i = 0; i < positions.length; i++) {  //if(positions){ ALSO WORKS BUT WHY?
     // draw ellipse at eyes position point [27], [32]
     var leftEyeX = positions[27][0];  //array index 27 y
     var leftEyeY = positions[27][1];  //array index 27 x
     ellipseMode(CENTER);
-    ellipse(leftEyeX, leftEyeY, 20, 10);
+    ellipse(leftEyeX, leftEyeY, 10, 10);
     
     var rightEyeX = positions[32][0];
     var rightEyeY = positions[32][1];
     ellipseMode(CENTER);
-    fill(0);
-    ellipse(rightEyeX, rightEyeY, 20, 10);
-    
-    //console.log("left eye pos = " + round(leftEyeX) + " " +round(leftEyeY));
+    ellipse(rightEyeX, rightEyeY, 10, 10);
   }
 
-  ripple++;
-  stroke(255,0,0);
-  noFill();
-  ellipse(leftEyeX, leftEyeY, ripple, ripple);
-  ellipse(rightEyeX, rightEyeY, ripple, ripple);
+  // ripple++;
+  // stroke(255,0,0);
+  // noFill();
+  // ellipse(leftEyeX, leftEyeY, ripple, ripple);
+  // ellipse(rightEyeX, rightEyeY, ripple, ripple);
 
-if (ripple > 100) ripple = 0;
+  // if (ripple > 100) ripple = 0;
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
